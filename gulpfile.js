09b6a53ade37b184b8b145ed674e8410e544a674
Lucas Fernando Amorim <lf.amorim@bipbop.com.br>
2015-07-24 11:12:11 -0300

    O BIPBOP-QL agora precisa apenas de um único .js para carregar todo o conteúdo, CSS, imagens e lógica JavaScript. Também foi corrigido um bug que era forçado parâmetros padrão quando enviados como parâmetros apenas.

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,48 @@
 var gulp = require("gulp"),
-        jsmin = require("gulp-uglify"),
-        sass = require("gulp-sass"),
-        minify = require("gulp-minify-css"),
-        autoprefixer = require("gulp-autoprefixer"),
-        pngquant = require("imagemin-pngquant"),
-        imagemin = require("gulp-imagemin"),
-        rename = require("gulp-rename");
+    jsmin = require("gulp-uglify"),
+    sass = require("gulp-sass"),
+    minify = require("gulp-minify-css"),
+    autoprefixer = require("gulp-autoprefixer"),
+    pngquant = require("imagemin-pngquant"),
+    imagemin = require("gulp-imagemin"),
+    cssBase64 = require('gulp-css-base64'),
+    css2js = require("gulp-css2js"),
+    concat = require("gulp-concat"),
+    rename = require("gulp-rename");
 
 gulp.task("build-images", function () {
-    return gulp.src("src/images/*")
+    return gulp.src("src/bipbop/**/*")
         .pipe(imagemin({
             progressive: true,
             use: [pngquant()]
         }))
-        .pipe(gulp.dest("dist/bipbop"));
+        .pipe(gulp.dest("temp/bipbop"));
 });
 
-gulp.task("build-style", function () {
+gulp.task("build-style", ['build-images'], function () {
     return gulp.src("src/jquery.bipbop.scss")
-            .pipe(sass())
-            .pipe(autoprefixer())
-            .pipe(gulp.dest("dist"))
-            .pipe(rename({
-                suffix: ".min"
-            }))
-            .pipe(minify())
-            .pipe(gulp.dest("dist"));
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest("temp"))
+        .pipe(rename({
+            suffix: ".min"
+        }))
+        .pipe(cssBase64())
+        .pipe(minify())
+        .pipe(css2js())
+        .pipe(rename("jquery.bipbop.style.js"))
+        .pipe(gulp.dest("temp"));
 });
 
-gulp.task("build-script", function () {
-    return gulp.src("src/jquery.bipbop.js")
-            .pipe(gulp.dest("dist"))
-            .pipe(jsmin())
-            .pipe(rename({
-                suffix: ".min"
-            }))
-            .pipe(gulp.dest("dist"));
+gulp.task("build-script", ["build-style"], function () {
+    return gulp.src(["temp/jquery.bipbop.style.js", "src/jquery.bipbop.js"])
+        .pipe(concat("jquery.bipbop.js"))
+        .pipe(gulp.dest("dist"))
+        .pipe(jsmin())
+        .pipe(rename({
+            suffix: ".min"
+        }))
+        .pipe(gulp.dest("dist"));
 });
 
 gulp.task("default", ["build-script", "build-style", "build-images"]);
